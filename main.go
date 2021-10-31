32a1423face902c64a4b968ac4b1a62beed7dc06
Quin Marilyn <quin.marilyn05@gmail.com>
2021-10-31 06:59:11 -0600

    go fmt

--- main.go
+++ main.go
@@ -11,15 +11,15 @@ func main() {
 		os.Exit(1)
 	}
 	if !fileExists(os.Args[1]) {
-		fmt.Println("Error: that file does not exist.");
-		os.Exit(1);
+		fmt.Println("Error: that file does not exist.")
+		os.Exit(1)
 	}
 }
 
 func fileExists(filename string) bool {
-    info, err := os.Stat(filename)
-    if os.IsNotExist(err) {
-        return false
-    }
-    return !info.IsDir()
+	info, err := os.Stat(filename)
+	if os.IsNotExist(err) {
+		return false
+	}
+	return !info.IsDir()
 }
