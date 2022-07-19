a4e53399a269a51ff7d6685b73fc7c9ff26febae
Quin <quin.marilyn05@gmail.com>
2022-07-19 12:21:48 -0600

    Basic argument parsing with argparse, handles input and output filenames.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/thequinbox/textify
 
 go 1.18
+
+require github.com/akamensky/argparse v1.3.1
--- main.go
+++ main.go
@@ -2,8 +2,18 @@ package main
 
 import (
 	"fmt"
+	"os"
+
+	"github.com/akamensky/argparse"
 )
 
 func main() {
-	fmt.Println("Welcome to Textify!")
+	parser := argparse.NewParser("textify", "Converts many common document types to plane text")
+	input := parser.String("i", "input", &argparse.Options{Required: true, Help: "File to convert"})
+	output := parser.String("o", "output", &argparse.Options{Required: false, Help: "File to write the output to. Default is input-file + .txt"})
+	err := parser.Parse(os.Args)
+	if err != nil {
+		fmt.Println(parser.Usage(err))
+	}
+	fmt.Println("Got input file of: " + *input + " and output file of " + *output)
 }
